f5d773a979cf06882871c4dfa841d3add37238e7
Joël Galeran <Jolg42@users.noreply.github.com>
2021-02-05 18:41:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const token = core.getInput('token');
+    const masterIssueId = core.getInput('dependency-dashboard-id');
+    core.info(`masterIssueId ${masterIssueId}`);
+    
+    const octokit = new github.GitHub(token);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const masterIssue = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}', {
+      ...github.context.repo,
+      issue_number: masterIssueId
+    })
+    
+    core.info(`Found masterIssue ${masterIssue}`);
+    core.info(`Found masterIssue body ${masterIssue.body}`);
+    
+    const CHECKED = '- [x] <!-- manual job -->' 
+    const UNCHECKED = '- [ ] <!-- manual job -->'
+    
+    if (masterIssue.body.contains(CHECKED)) {
+      return 'Already Checked'
+    }
+    
+    await octokit.request('PATCH /repos/{owner}/{repo}/issues/{issue_number}', {
+      ...github.context.repo,
+      issue_number: masterIssueId,
+      body: masterIssue.body.replace(UNCHECKED, CHECKED)
+    })
+  
   } catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+run();
+
+
+
+    const response = await octokit.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split(',') : undefined
+    })
+
+    core.setOutput('issue', JSON.stringify(response.data));
+
+  } catch (err) {
+    core.setFailed(err.message)
   }
 }
 
