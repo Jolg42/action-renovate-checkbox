885d562c31870a9157544c22cc950c63892a8f99
Alex Babel <alexander.babel@icloud.com>
2022-01-31 12:04:15 +0100

    feat: add support for owner, repo and token input

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ const UNCHECKED = "- [ ] <!-- manual job -->";
 async function run() {
   try {
     const masterIssueId = core.getInput("master-issue-id");
-    const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+    const owner = core.getInput("owner");
+    const repo = core.getInput("repo");
+    const token = core.getInput("token");
+
+    if (!masterIssueId) {
+      core.setFailed("master-issue-id is missing! Please take a look at the documentation and set the missing parameter.");
+      return;
+    }
+
+    if (!(token ? token : process.env.GITHUB_TOKEN)) {
+      core.setFailed("token and GITHUB_TOKEN is missing! Please take a look at the documentation and set the missing parameter.");
+      return;
+    }
+
+    const octokit = github.getOctokit(token ? token : process.env.GITHUB_TOKEN);
 
     const { data: masterIssue } = await octokit.issues.get({
-      ...github.context.repo,
+      owner: owner ? owner : github.context.repo.owner,
+      repo: repo ? repo : github.context.repo.repo,
       issue_number: masterIssueId,
-    });
+    }).catch(err => core.error(err));
 
     // if (masterIssue.user.login !== "renovate[bot]") {
     //   const message = `Issue ID ${masterIssue.id} author must be "renovate[bot]"`;
@@ -26,7 +41,8 @@ async function run() {
     }
 
     await octokit.issues.update({
-      ...github.context.repo,
+      owner: owner ? owner : github.context.repo.owner,
+      repo: repo ? repo : github.context.repo.repo,
       issue_number: masterIssueId,
       body: masterIssue.body.replace(UNCHECKED, CHECKED),
     });
